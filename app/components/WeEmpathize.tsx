0d467d6829ad3b145e67406cc8adf413b3ac85a1
Swam Htet Aung <swamhtetaungg@gmail.com>
2024-11-18 12:41:21 +0700

    fix: move the at is ness card to top left

--- app/components/WeEmpathize.tsx
+++ app/components/WeEmpathize.tsx
@@ -8,7 +8,7 @@ const WeEmpathize = () => {
   return (
     <Container
       includePaddingBlock
-      className="relative w-full min-h-[600px] lg:min-h-[800px] xl:min-h-[900px] flex items-end"
+      className="relative w-full min-h-[600px] lg:min-h-[800px] xl:min-h-[900px] flex items-start"
     >
       {/* Overlay */}
       <div className="absolute inset-0 bg-gradient-to-b from-black/10 to-black/70 z-10"></div>
@@ -18,7 +18,7 @@ const WeEmpathize = () => {
         alt="image of a mom breast feeding her child"
         className="object-cover"
       ></Image>
-      <div className="relative w-full xl:w-2/5 p-[24px] lg:p-[32px] bg-isness-white z-20 space-y-[8px] rounded-lg shadowlg">
+      <div className="relative w-full xl:w-2/5 p-[24px] lg:p-[32px] bg-isness-white z-20 space-y-[8px] rounded-lg shadow-lg">
         <MainHeading className="tracking-wider">At isness</MainHeading>
         <ParagraphText>
           We deeply understand the care, effort, and love that goes into
